ab854c81718e2e0ba28df14284b242fd9e711a78
AstateI <alexismoh54@gmail.com>
2023-09-23 15:35:52 +0200

    mise en place

---
+++ tetris.py
@@ -0,0 +1,11 @@
+# ouvrir une fenetre
+# menu du jeu (start option quitter)
+# start = générer tous les objet du "jeu" (interfaceet tout ce qui fais le jeu)
+# pendant le jeu (temps, score, règle du jeu)
+# fin du jeu (affichage scor, bouton recommencer, bouton quitter)
+
+n = int () #varriable def f(x = int ()): fonction f(x)
+nombre = int () #varriable
+n = int () #varriable
+
+def OpenWindows() #fonction
